c485760a45abd76ef1d3a86259e3b283048acbac
CL0SeY <christopher.close@gmail.com>
2020-04-22 12:26:46 +1000

    Got some togglin' happening.

--- run.sh
+++ run.sh
@@ -8,6 +8,8 @@ RTMP_STREAM_NAMES=${RTMP_STREAM_NAMES-live,testing}
 RTMP_STREAMS=$(echo ${RTMP_STREAM_NAMES} | sed "s/,/\n/g")
 RTMP_PUSH_URLS=$(echo ${RTMP_PUSH_URLS} | sed "s/,/\n/g")
 
+echo $RTMP_PUSH_URLS
+
 apply_config() {
 
     echo "Creating config"
